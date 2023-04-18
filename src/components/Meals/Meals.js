0b47c43496c2ca2acad9de4c28436777e8778a08
shivam2399 <dubey.shivam2399@gmail.com>
2023-04-18 20:47:29 +0530

    Rendering meal items

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,9 +1,11 @@
 import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
 
 const Meals = () => {
     return (
         <>
          <MealsSummary />
+         <AvailableMeals />
         </>
     )
 }

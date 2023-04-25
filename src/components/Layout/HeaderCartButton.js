a2ef27ae559753a8cadede6e4faab4be7f070859
shivam2399 <dubey.shivam2399@gmail.com>
2023-04-25 21:32:49 +0530

    updating cart items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,27 @@
+import { useContext } from "react";
+
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = props => {
+    const cartCntxt = useContext(CartContext);
+    let quantity = 0;
+    cartCntxt.items.forEach(item => {
+        quantity += Number(item.quantity);
+    })
+
+    // const numberOfCartItems = cartCntx.items.reduce((currNumber, item) => {
+    //     return currNumber + item.amount;
+    // }, 0);
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
               <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{quantity}</span>
         </button>
     )
 }
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,25 @@
+import React, { useContext } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
+import CartCntx from '../../../store/cart-context';
+
+
 
 const MealItemForm = props => {
+   const cartcntx = useContext(CartCntx)
+   console.log('reinitialized cartcntx', cartcntx)
+   const addItemToCart = (event) => {
+    event.preventDefault();
+    //update the cartcntx.item
+    // cartcntx.items.push(props.item)
+    const quantity = document.getElementById('amount_' + props.id).value;
+    cartcntx.addItem({...props.item , quantity: quantity});
+    console.log('after adding', cartcntx)
+   }
+
     return (
         <form classes={classes.form}>
+          {console.log('inside render' ,cartcntx.items)}
         <Input 
           label="Amount" 
           input={{
@@ -15,7 +31,7 @@ const MealItemForm = props => {
             defaultValue: '1'
            }} 
         />
-        <button className={classes.button}>+ Add</button>
+        <button onClick={addItemToCart} className={classes.button}>+ Add</button>
         </form>
     )
 }
